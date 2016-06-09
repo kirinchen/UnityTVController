745aa09515e512ef4e745aa072bde600d30bc823
kirinchen <a29243685@msn.com>
2016-06-09 16:48:05 +0800

    add tv grid

--- Script/TvGridGroup.cs
+++ Script/TvGridGroup.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 namespace TVController {
+    [RequireComponent(typeof(GridLayoutGroup))]
     public class TvGridGroup : TVBehaviour {
 
-
+        private GridLayoutGroup gridLayout;
+        private TvGrid[][] gridMtx;
 
         new void Awake() {
-
+            gridLayout = GetComponent<GridLayoutGroup>();
         }
 
         public void loadGrids() {
